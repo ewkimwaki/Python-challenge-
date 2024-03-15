d14014183f5b9eb6c57914e2f9de736f46bed448
ewkimwaki <153472553+ewkimwaki@users.noreply.github.com>
2024-03-15 09:36:40 +0300

    Update secondChallenge.py
    The number in the map only gets updated if the current number is greater than the existing one.

--- moringa Challenge/secondChallenge.py
+++ moringa Challenge/secondChallenge.py
@@ -1,5 +1,5 @@
 def digit_sum(num):
-    return sum(int(digit) for digit in str(num))
+    return sum(int(digit) for digit in str(abs(num)))  # Always use the absolute value
 
 def solution(A):
     digit_sums = {}
@@ -12,11 +12,11 @@ def solution(A):
             pair_sum = digit_sums[current_sum] + num
             max_sum = max(max_sum, pair_sum)
         else:
-            digit_sums[current_sum] = num
+            digit_sums[current_sum] = max(digit_sums.get(current_sum, float('-inf')), num)
 
     return max_sum
 
 # Test examples
 print(f'Test 1: {solution([51, 71, 17, 42])}')
-print(f'Test 2: {solution([42, 33, 60])}') 
-print(f'Test 3:{solution([51, 32, 43])}')
+print(f'Test 2: {solution([42, 33, 60])}')
+print(f'Test 3: {solution([51, 32, 43])}')
